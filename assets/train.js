d6270ead06d6dc7aea62fded22c544825a13e9fd
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2017-12-20 11:26:46 -0500

    push

---
+++ assets/train.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    console.log("test");
+
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB6BDUblNm3Pb4DBTAbxd5Jn1m7CK2dPp8",
+    authDomain: "train-schedule-cf4cc.firebaseapp.com",
+    databaseURL: "https://train-schedule-cf4cc.firebaseio.com",
+    projectId: "train-schedule-cf4cc",
+    storageBucket: "",
+    messagingSenderId: "536089065403"
+  };
+  firebase.initializeApp(config)
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+  

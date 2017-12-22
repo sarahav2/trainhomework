04fae49961f141547a7377e4387221c4f4dec523
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2017-12-22 07:18:34 -0500

    push

--- assets/train.js
+++ assets/train.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     console.log("test");
 
-//     // Initialize Firebase
-//   var config = {
-//     apiKey: "AIzaSyB6BDUblNm3Pb4DBTAbxd5Jn1m7CK2dPp8",
-//     authDomain: "train-schedule-cf4cc.firebaseapp.com",
-//     databaseURL: "https://train-schedule-cf4cc.firebaseio.com",
-//     projectId: "train-schedule-cf4cc",
-//     storageBucket: "",
-//     messagingSenderId: "536089065403"
-//   };
-//   firebase.initializeApp(config)
+   // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB6BDUblNm3Pb4DBTAbxd5Jn1m7CK2dPp8",
+    authDomain: "train-schedule-cf4cc.firebaseapp.com",
+    databaseURL: "https://train-schedule-cf4cc.firebaseio.com",
+    projectId: "train-schedule-cf4cc",
+    storageBucket: "",
+    messagingSenderId: "536089065403"
+  };
+  firebase.initializeApp(config)
 
   // declare database var
   database = firebase.database();
